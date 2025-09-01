6817868830b0ea249bd281274e70db23b7aac657
bd1ogo <barrosof.diogo@gmail.com>
2025-09-01 18:39:11 -0300

    Todos os erros de conexão resolvidos

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
     $host = '127.0.0.1';
     $db = 'empresa';
     $user = 'root';
-    $pass = 'Passw0rd';
+    $pass = '';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
